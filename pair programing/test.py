bdb99cf377b4da23ad9896f4fbd3e23a8f7d540c
Zhou Zhi <zhouzhi1995@hotmail.com>
2025-03-08 22:43:08 +0800

    iter_4

--- pair programing/test.py
+++ pair programing/test.py
@@ -2,9 +2,10 @@ import pytest
 import numpy as np
 import pandas as pd
 from source import ai_fraud_detection_model
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,f1_score
 
 sample_data = pd.read_csv('test_data.csv')
+sample_test = sample_data.drop('fraud')
 sample_result = sample_data['fraud']
 
 def return_test():
@@ -19,3 +20,8 @@ def accuracy_test():
 	y_true = sample_result
 	y_pred = sample_data.apply(lambda x: ai_fraud_detection_model(x), axis=1)
 	assert accuracy_score(y_true, y_pred) > 0.5
+
+def f1_test():
+	y_true = sample_result
+	y_pred = sample_test.apply(lambda x: ai_fraud_detection_model(x), axis=1)
+	assert f1_score(y_true, y_pred) > 0.9	
