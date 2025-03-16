d3edd4a5d0cab01fe9ee9718e094a7ab4e1c99a9
Zhou Zhi <zhouzhi1995@hotmail.com>
2025-03-16 10:46:57 +0800

    refactor

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import argparse
-from utils import *
+from utils import threat_identification,threat_classification,threat_notifications
 
 def bandguardAI(method, data,target_email):
+	'''
+	call various function by given method
+	all -> None Output
+	identification -> Boolean
+	classification -> Tuple (String, Int)
+	'''
 
-	if method == 'all':
-		is_threat = threat_identification(data)
-		threat_cate, threat_level = threat_classification(data,is_threat)
-		return threat_notifications(data,is_threat,threat_cate,threat_level,target_email)
+    if method == 'all':
+        is_threat = threat_identification(data)
+        threat_cate, threat_level = threat_classification(data,is_threat)
+        threat_notifications(data,is_threat,threat_cate,threat_level,target_email)
 
+    elif method == 'identification':
+        return threat_identification(data)
 
-	if method == 'identification':
-		return threat_identification(data)
+    elif method == 'classification':
+        return threat_classification(data,is_threat)
 
-	if method == 'classification':
-		return threat_classification(data,is_threat)
-
-	return raise "no such method"
+    else:
+        raise "no such method"
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
                     prog='BankGuard AI CLI',
                     epilog='This is an app for BankGuard AI')
-	parser.add_argument('-m','--method')           # positional argument
-	parser.add_argument('-d', '--data')    
-	parser.add_argument('-e', '--target_email')    
+    parser.add_argument('-m','--method')           # positional argument
+    parser.add_argument('-d', '--data')    
+    parser.add_argument('-e', '--target_email')    
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	# data = json.loads(args.data)
-	data = args.data
-	print(data)
+    # data = json.loads(args.data)
+    data = args.data
 
-	bandguardAI(args.method, args.data,args.target_email)
+    # call main function
+    bandguardAI(args.method, args.data,args.target_email)
